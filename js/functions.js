9863965a0a1c33726593c5af667113b7c5a41586
angelslr <angelgsalazar@gmail.com>
2020-05-16 18:55:30 -0300

    Correccion de errores y nombres

--- js/functions.js
+++ js/functions.js
@@ -23,24 +23,24 @@ function changeTheme() {
     }
 }
 
-var cronometro;
-function detenerse() {
-    clearInterval(cronometro);
-    cronometro = 0;
-    document.getElementById("h1").innerHTML = "Prueba finalizada";
-}
-
 function restart() {
-    if (cronometro != 0) {
-        var answer = confirm("Prueba en ejecucion, ¿desea volver al inicio? (Si lo hace, la prueba se detendra)");
+    if (cron != 0) {
+        var answer = confirm("Prueba en ejecución, ¿desea volver al inicio? (Si lo hace, la prueba se detendrá)");
         if (answer == true) {
-            detenerse();
-            location.replace("index.html")
+            stopCron();
+            location.replace("index.html");
         }
     }
     else {
-        location.replace("index.html")
+        location.replace("index.html");
     }
+}
+
+var cron;
+function stopCron() {
+    clearInterval(cron);
+    cron = 0;
+    document.getElementById("h1").innerHTML = "Prueba finalizada";
 }
 
 function load() {
@@ -49,7 +49,7 @@ function load() {
     s = document.getElementById("segundos");
     m = document.getElementById("minutos");
 
-    cronometro = setInterval(
+    cron = setInterval(
         function () {
             if (contador_s == 60) {
                 contador_s = 0;
@@ -65,24 +65,22 @@ function load() {
         }
         , 1000);
 }
+
 n = 1;
 function generateTable() {
-    a = 0;
-    while (a < 10) {
-        var table = document.getElementById("tbResults");
-        var row = table.insertRow(-1);
-        var nro = row.insertCell(0);
-        var farmaco = row.insertCell(1);
-        var duracion = row.insertCell(2);
-        var pasos = row.insertCell(3)
-        var observaciones = row.insertCell(4);
+    var table = document.getElementById("tbResults");
+    var row = table.insertRow(-1);
+    var nro = row.insertCell(0);
+    var farmaco = row.insertCell(1);
+    var duracion = row.insertCell(2);
+    var pasos = row.insertCell(3)
+    var observaciones = row.insertCell(4);
 
-        nro.innerHTML = n++;
-        farmaco.innerHTML = document.getElementById("ffarmaco").value;
-        duracion.innerHTML = "5:40";
-        pasos.innerHTML = "500"
-        observaciones.innerHTML = "Evidente disminucion de la actividad"
+    nro.innerHTML = n++;
+    farmaco.innerHTML = document.getElementById("ffarmaco").value;
+    duracion.innerHTML = document.getElementById("fduracion").value;
+    pasos.innerHTML = document.getElementById("fpasos").value;
+    observaciones.innerHTML = document.getElementById("fobservaciones").value;
 
-        a++;
-    }
+    a++;
 }
