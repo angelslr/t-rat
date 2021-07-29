a872bcf6dcfedbf1762faaef1162b05e516b35bb
Angel Salazar <angelgsalazar@gmail.com>
2021-07-29 17:54:22 -0300

    statistics translated

--- js/chartContainer.js
+++ js/chartContainer.js
@@ -17,64 +17,6 @@ const borColor = [
 const backColor = [
 	"rgba(37, 204, 247, 0.2)", "rgba(253, 114, 114, 0.2)", "rgba(84, 160, 255, 0.2)", "rgba(0, 210, 211, 0.2)", "rgba(26, 188, 156, 0.2)", "rgba(46, 204, 113, 0.2)", "rgba(52, 152, 219, 0.2)", "rgba(155, 89, 182, 0.2)", "rgba(52, 73, 94, 0.2)", "rgba(22, 160, 133, 0.2)", "rgba(39, 174, 96, 0.2)", "rgba(41, 128, 185, 0.2)", "rgba(142, 68, 173, 0.2)", "rgba(44, 62, 80, 0.2)", "rgba(241, 196, 15, 0.2)", "rgba(230, 126, 34, 0.2)", "rgba(231, 76, 60, 0.2)", "rgba(236, 240, 241, 0.2)", "rgba(149, 165, 166, 0.2)", "rgba(243, 156, 18, 0.2)", "rgba(211, 84, 0, 0.2)", "rgba(192, 57, 43, 0.2)", "rgba(189, 195, 199, 0.2)", "rgba(127, 140, 141, 0.2)", "rgba(85, 239, 196, 0.2)", "rgba(129, 236, 236, 0.2)", "rgba(116, 185, 255, 0.2)", "rgba(162, 155, 254, 0.2)", "rgba(223, 230, 233, 0.2)", "rgba(0, 184, 148, 0.2)", "rgba(0, 206, 201, 0.2)", "rgba(9, 132, 227, 0.2)", "rgba(108, 92, 231, 0.2)", "rgba(255, 234, 167, 0.2)", "rgba(250, 177, 160, 0.2)", "rgba(255, 118, 117, 0.2)", "rgba(253, 121, 168, 0.2)", "rgba(253, 203, 110, 0.2)", "rgba(225, 112, 85, 0.2)", "rgba(214, 48, 49, 0.2)", "rgba(254, 202, 87, 0.2)", "rgba(95, 39, 205, 0.2)", "rgba(84, 160, 255, 0.2)", "rgba(1, 163, 164, 0.2)"
 ]
-/*var data;
-(
-	async () => {
-		data = (await dataToChart())
-	}
-)()*/
-
-/*var ctx = document.getElementById('chartRegByCD').getContext('2d');
-var chartRegByCD = new Chart(ctx, {
-	type: 'bar',
-	data: {
-		labels: ['Control', 'Droga'],
-		datasets: [{
-			label: '# de registros',
-			data: [0],
-			backgroundColor: [
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(255, 159, 64, 0.2)'
-			],
-			borderColor: [
-				'rgba(255, 99, 132, 1)',
-				'rgba(255, 159, 64, 1)'
-			],
-			borderWidth: 1
-		}]
-	},
-	options: {}
-});
-*/
-
-/*var ctx = document.getElementById('chartDrugs').getContext('2d');
-var chartDrugs = new Chart(ctx, {
-	type: 'bar',
-	data: {
-		labels: ['-'],
-		datasets: [{
-			label: 'Drogas usadas',
-			data: [0],
-			backgroundColor: backColor,
-			borderColor: borColor,
-			borderWidth: 1
-		}]
-	},
-	options: {
-		scales: {
-			yAxes: [{
-				ticks: {
-					stepSize : 1
-				},
-				scaleLabel: {
-					display: true,
-					labelString: 'Cantidad de Registros'
-				}
-			}]
-		}
-	}
-});
-*/
 
 var ctx = document.getElementById('chartDateData').getContext('2d');
 var chartDateData = new Chart(ctx, {
@@ -82,7 +24,7 @@ var chartDateData = new Chart(ctx, {
 	data: {
 		labels: ['-'],
 		datasets: [{
-			label: 'ID Registro / Movimiento',
+			label: 'Test ID / Movement',
 			backgroundColor: backColor,
 			borderColor: borColor,
 			data: [0],
@@ -99,7 +41,7 @@ var chartDateData = new Chart(ctx, {
 					}
 					let opt = document.getElementById('selectOpt');
 					let text = opt.options[opt.selectedIndex].text;
-					return 'Fecha Registro / ' + text + ': ' + value;
+					return 'Test date / ' + text + ': ' + value;
 				}
 			}
 		},
@@ -113,7 +55,7 @@ var chartDateData = new Chart(ctx, {
 			yAxes: [{
 				scaleLabel: {
 					display: true,
-					labelString: 'Movimiento/Duración'
+					labelString: 'Movement/Duration'
 				}
 			}]
 		}
@@ -126,7 +68,7 @@ var chartResearchers = new Chart(ctx, {
 	data: {
 		labels: ['-'],
 		datasets: [{
-			label: 'Pruebas realizadas por investigadores',
+			label: 'Tests performed by researchers',
 			data: [0],
 			backgroundColor: backColor,
 			borderColor: borColor,
@@ -141,7 +83,7 @@ var chartResearchers = new Chart(ctx, {
 				},
 				scaleLabel: {
 					display: true,
-					labelString: 'Nro de pruebas realizadas'
+					labelString: 'Number of tests performed'
 				}
 			}]
 		}
@@ -154,7 +96,7 @@ var chartDrugsMov = new Chart(ctx, {
 	data: {
 		labels: ['-'],
 		datasets: [{
-			label: 'Relación Movimiento/Droga',
+			label: 'Movement/Drug Ratio',
 			data: [{
 				label: "Blue",
 				backgroundColor: "blue",
@@ -178,28 +120,10 @@ var chartDrugsMov = new Chart(ctx, {
 			yAxes: [{
 				scaleLabel: {
 					display: true,
-					labelString: 'Cantidad de movimiento'
+					labelString: 'Amount of movement'
 				}
 			}]
 		}
-		/*scales: {
-			yAxes: [{
-				gridLines: {
-					display: false
-				},
-				ticks: {
-					display: false
-				}
-			}],
-			xAxes: [{
-				gridLines: {
-					display: false
-				},
-				ticks: {
-					display: false
-				}
-			}]
-		}*/
 	}
 });
 
@@ -209,7 +133,7 @@ var chartDrugsMovByDose = new Chart(ctx, {
 	data: {
 		labels: ['-'],
 		datasets: [{
-			label: 'Relación Movimiento/Droga',
+			label: 'Movement/Drug Ratio',
 			data: [{
 				label: "Blue",
 				backgroundColor: "blue",
@@ -233,43 +157,21 @@ var chartDrugsMovByDose = new Chart(ctx, {
 			yAxes: [{
 				scaleLabel: {
 					display: true,
-					labelString: 'Cantidad de movimiento'
+					labelString: 'Amount of movement'
 				}
 			}]
 		}
-		/*scales: {
-			yAxes: [{
-				gridLines: {
-					display: false
-				},
-				ticks: {
-					display: false
-				}
-			}],
-			xAxes: [{
-				gridLines: {
-					display: false
-				},
-				ticks: {
-					display: false
-				}
-			}]
-		}*/
 	}
 });
 
 
 getDataForCharts();
 
-
-
 async function getDataForCharts() {
 	let results;
 	jsonREGS = await toBackGETAll(epRegAll);
 	jsonDRUGS = await toBackGETAll(epDrugAll);
 	jsonRES = await toBackGETAll(epReserAll);
-
-	//await dataChartDrugs();
 
 	fillDateData();
 
@@ -283,16 +185,6 @@ async function getDataForCharts() {
 	updateDataLabelDrugs(results, chartDrugsMovByDose);
 }
 
-/*function addElement() {
-	let results = addDrugToChart();
-	updateReplaceDataLabel(results, chartDrugs);
-}
-
-function removeElement() {
-	let results = removeDrugItemChart();
-	updateReplaceDataLabel(results, chartDrugs);
-}*/
-
 function fillDateData() {
 	results = dataDateChart();
 	updateReplaceDataLabel(results, chartDateData);
@@ -302,7 +194,6 @@ function dateFilter() {
 	let results = dateFilterChart();
 	updateReplaceDataLabel(results, chartDateData);
 }
-
 
 function updateReplaceDataLabel(results, chart) {
 	chart.data.labels = results[0];
